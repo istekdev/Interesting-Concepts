dd2c5225eba86fe4c5d347cb9b4411089bd82fe3
ISTEK <tamdacoder@gmail.com>
2025-09-25 08:58:19 -0500

    Update experiment_3.py

--- experiment_3.py
+++ experiment_3.py
@@ -2,14 +2,12 @@
 
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from termcolor import colored
-import time
+from eth_utils import keccak
 import os
 
 password = input(colored("WHAT WILL BE YOUR PASSWORD: ", "blue", attrs=["bold"]))
 nonce = os.urandom(12)
-obj = AESGCM(password.encode("utf-8"))
+obj = AESGCM(keccak(password.encode("utf-8")))
 item = input(colored("WHAT DO YOU WANT TO ENCRYPT: ", "blue", attrs=["bold"]))
 enc = obj.encrypt(nonce, item.encode("utf-8"), None)
 print(colored(enc.hex(), "green", attrs=["bold"]))
-
-
